1de94b673a8352fc9a24aebda84c556debc57900
vsymnyivs <vsymnyivs@gmail.com>
2024-01-21 17:42:47 +0300

    write function get_executed_operations(value)

--- src/utils.py
+++ src/utils.py
@@ -9,3 +9,13 @@ def load_json_file():
     return json_data
 
 
+def get_executed_operations(values):
+    executed_operations = []
+    for value in values:
+        if value == {}:
+            continue
+        else:
+            executed_operations.append(value)
+    return executed_operations
+
+
