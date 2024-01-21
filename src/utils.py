39cc698f0b4cd3d09fbeb49e6c3a90dc8ffc4857
vsymnyivs <vsymnyivs@gmail.com>
2024-01-21 19:10:18 +0300

    Написал докстринги функциям, привет функцию sort_operations() к требованиям PEP8

--- src/utils.py
+++ src/utils.py
@@ -4,12 +4,18 @@ from datetime import datetime
 
 
 def load_json_file():
+    """"
+    Функция принимает json_file и возвращает словарь
+    """
     with open(OPERATIONS, encoding="utf-8") as file:
         json_data = json.load(file)
     return json_data
 
 
 def get_executed_operations(values):
+    """
+    Функция фильтрует список оставляя удачновыполненные операции
+    """
     executed_operations = []
     for value in values:
         if value == {}:
@@ -20,16 +26,19 @@ def get_executed_operations(values):
 
 
 def sort_operations(data):
+    """
+    Функция ждя сортировки по дате и форматирования вывода даты
+    """
     date_operations = []
     for sort_date in data:
-        sort_operations = datetime.strptime(sort_date["date"], "%Y-%m-%dT%H:%M:%S.%f")
-        formate_date = f"{sort_operations:%d.%m.%Y}"
+        sorted_operations = datetime.strptime(sort_date["date"], "%Y-%m-%dT%H:%M:%S.%f")
+        formate_date = f"{sorted_operations:%d.%m.%Y}"
         date_operations.append(formate_date)
     return date_operations
 
 
-def slide_operations(operation):
+def slide_operations():
+    pass
 
 
-
-#print(sort_operations(get_executed_operations(load_json_file())))
+# print(sort_operations(get_executed_operations(load_json_file())))
